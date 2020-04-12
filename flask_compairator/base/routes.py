838bcf41f8ce8c8370f99d55eba9bfc65fd225d9
Kevin Li <kli@cern.ch>
2020-04-12 23:01:43 +0200

    Actual python interaction with re-rendering.

--- flask_compairator/base/routes.py
+++ flask_compairator/base/routes.py
@@ -1,3 +1,4 @@
+import numpy as np
 from flask import render_template
 
 from . import blueprint
@@ -5,10 +6,11 @@ from . import blueprint
 
 @blueprint.route('/index')
 def index():
-    return render_template("index.html", var=None)
+    return render_template("index.html", label=None)
 
 
-@blueprint.route('/python_process')
+@blueprint.route('/python_process', methods=['POST'])
 def python_process():
-    print("Hello! You're in a python function now!")
-    return ("Nothing")
+    r = np.random.randint(1, 10, 1)[0]
+    print(f"Hello! You're in a python function now! Your new number picked is: {r}.")
+    return render_template("index.html", label=str(r))
