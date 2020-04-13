bc6c93416c3d117785e221574e4027c53c557116
Kevin Li <kli@cern.ch>
2020-04-13 23:59:38 +0200

    Improve game a little.

--- flask_compairator/base/routes.py
+++ flask_compairator/base/routes.py
@@ -7,6 +7,7 @@ from flask import url_for
 from . import blueprint
 
 tries = 0
+r = np.random.randint(1, 6, 1)[0]
 animals = dict(cat=1, dog=2, dragon=3, dove=4, spider=5)
 
 
@@ -17,22 +18,24 @@ def index():
 
 @blueprint.route('/python_process', methods=['GET', 'POST'])
 def python_process():
-    global tries
+    global tries, r
     if request.method == 'POST':
-        rnum = np.random.randint(1, 5, 1)[0]
+        if tries > 2:
+            tries = 0
+            r = np.random.randint(1, 6, 1)[0]
+            print(np.random.randint(1, 6, 10))
+            return redirect(url_for('home_blueprint.over'))
+
         for key, name in request.form.items():
-            if animals.get(name) == rnum:
+            if animals.get(name) == r:
                 tries = 0
-                text = f"Yes! You found your {name}!"
+                r = np.random.randint(1, 6, 1)[0]
+                text = f"Yes! You found a {name}!"
                 return render_template("index.html", label=text, color="w3-green")
             else:
                 tries = tries + 1
-                if tries > 3:
-                    tries = 0
-                    return redirect(url_for('home_blueprint.over'))
-                else:
-                    text = f"No - {name} not found... try again!"
-                    return render_template("index.html", label=text, color="w3-red")
+                text = f"Sorry, no {name} here... try again!"
+                return render_template("index.html", label=text, color="w3-red")
 
     elif request.method == 'GET':
         return render_template("index.html")
