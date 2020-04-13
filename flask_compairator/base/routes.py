e404d48ede742e00b016fb00f077e815b4436af1
Kevin Li <kli@cern.ch>
2020-04-13 13:50:39 +0200

    First set of buttons - redirecting to secret page not working.

--- flask_compairator/base/routes.py
+++ flask_compairator/base/routes.py
@@ -1,9 +1,12 @@
 import numpy as np
+from flask import redirect
 from flask import render_template
 from flask import request
+from flask import url_for
 
 from . import blueprint
 
+tries = 0
 animals = dict(cat=1, dog=2, dragon=3, dove=4, spider=5)
 
 
@@ -13,23 +16,24 @@ def index():
 
 
 @blueprint.route('/python_process', methods=['GET', 'POST'])
-def python_process(tries=0):
+def python_process():
+    global tries
     if request.method == 'POST':
         rnum = np.random.randint(1, 5, 1)[0]
-        name = request.form.get('value')
-        print(request.form, request.args, request.form.get('cat'), name)
-        # print(f"Hello! You're in a python function now! Your new number picked is: {r}.")
+        for key, name in request.form.items():
+            print(key, name, animals.get(name), rnum, tries)
 
-        if animals.get(name) == rnum:
-            text = f"Yes! You found your {name}!"
-            return render_template("index.html", label=text, type="w3-green")
-        else:
-            tries += 1
-            if tries > 3:
-                return render_template("")
+            if animals.get(name) == rnum:
+                tries = 0
+                text = f"Yes! You found your {name}!"
+                return render_template("index.html", label=text, color="w3-green")
             else:
-                text = f"No - {name} not found... try again!"
-                return render_template("index.html", label=text, type="w3-red")
+                tries = tries + 1
+                if tries > 3:
+                    return redirect(url_for('home'))
+                else:
+                    text = f"No - {name} not found... try again!"
+                    return render_template("index.html", label=text, color="w3-red")
 
     elif request.method == 'GET':
         return render_template("index.html")
