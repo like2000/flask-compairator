a842faa65314a532912b27f639bc04c44f8f1800
Kevin Li <kli@cern.ch>
2020-04-13 11:56:27 +0200

    On the way to get button interactions to work via flask request.

--- flask_compairator/base/routes.py
+++ flask_compairator/base/routes.py
@@ -1,7 +1,10 @@
 import numpy as np
 from flask import render_template
+from flask import request
 
 from . import blueprint
+
+animals = dict(cat=1, dog=2, dragon=3, dove=4, spider=5)
 
 
 @blueprint.route('/index')
@@ -9,8 +12,24 @@ def index():
     return render_template("index.html", label=None)
 
 
-@blueprint.route('/python_process', methods=['POST'])
-def python_process():
-    r = np.random.randint(1, 10, 1)[0]
-    print(f"Hello! You're in a python function now! Your new number picked is: {r}.")
-    return render_template("index.html", label=str(r))
+@blueprint.route('/python_process', methods=['GET', 'POST'])
+def python_process(tries=0):
+    if request.method == 'POST':
+        rnum = np.random.randint(1, 5, 1)[0]
+        name = request.form.get('value')
+        print(request.form, request.args, request.form.get('cat'), name)
+        # print(f"Hello! You're in a python function now! Your new number picked is: {r}.")
+
+        if animals.get(name) == rnum:
+            text = f"Yes! You found your {name}!"
+            return render_template("index.html", label=text, type="w3-green")
+        else:
+            tries += 1
+            if tries > 3:
+                return render_template("")
+            else:
+                text = f"No - {name} not found... try again!"
+                return render_template("index.html", label=text, type="w3-red")
+
+    elif request.method == 'GET':
+        return render_template("index.html")
